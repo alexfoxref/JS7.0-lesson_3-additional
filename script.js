d9608438c08c919a91f186efc658b3f65852e716
Alexander Demidenko <alexfoxref@gmail.com>
2019-04-23 02:28:47 +0500

    lesson-3 additional task

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict'
+
+let str = "урок-3-был слишком легким",
+    strSlice = "легким";
+
+// 1)
+str = str[0].toUpperCase() + str.slice(1);
+
+// 2)
+while ( ~str.indexOf("-") ) {
+    str = str.slice( 0, str.indexOf("-") ) + " " + str.slice( str.indexOf("-") + 1 );
+}
+console.log(str);
+
+// 3)
+str = str.slice( 0, str.indexOf(strSlice) ) + str.slice( str.indexOf(strSlice) + strSlice.length );
+strSlice = strSlice.slice(0, -2) + "о";
+alert(strSlice);
+
+// 4)
+let arr = [20, 33, 1, "Человек", 2, 3],
+    total = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if ( typeof(arr[i]) === 'number' ) {
+        total = total + Math.pow(arr[i], 3);
+    }
+}
+console.log(Math.sqrt(total));
+
+// 5)
+function beautifyString (string) {
+    if ( typeof(string) !== 'string' ) {
+        alert("Была введена не строка");
+    } else {
+        if (string.indexOf(" ") == 0) {
+            while (string.indexOf(" ") == 0) {
+                string = string.slice(1);
+            }
+        }
+        if ( string.lastIndexOf(" ") == (string.length - 1) ) {
+            while ( string.lastIndexOf(" ") == (string.length - 1) ) {
+                string = string.slice(0, -1);
+            }
+        }
+        if (string.length > 50) {
+            string = string.slice(0, 51) + "...";
+        }
+        alert(string);
+    }
+}
+// beautifyString("   Пр ивhgf ет   вап   ");
